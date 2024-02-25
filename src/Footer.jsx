1b98b086462bd3eb6bdd1ffe9d144efafd84dae8
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-02-25 10:23:49 +0800

    added bootstrap styling to header and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,9 +7,9 @@ function Copyright() {
 
     return (
         <>
-        <div className="container bg-dark">
-            <h5 className='text-light'>Developed by Neil Martin Gamboa</h5>
-            <h5 className='text-light'>©2024 BASE-404</h5>
+        <div className="container bg-dark p-3 my-auto">
+            <h5 className='text-primary d-flex justify-content-center'>Developed by Neil Martin Gamboa</h5>
+            <h5 className='text-primary d-flex justify-content-center'>©2024 BASE-404</h5>
         </div>
         </>
     )
--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,10 @@ function Title() {
 
     return (
         <>
-        <h1 className='text-primary'>React Demonstration</h1>
+        <div className='container-fluid bg-dark'>
+         <h1 className='text-primary d-flex justify-content-center p-3 fw-bold'>💻React Demonstration</h1>
+         <small className='text-primary d-flex justify-content-center mb-5'>Activity 13 - React Components, Props, and useState </small>
+        </div>
         </>
     )
 }
